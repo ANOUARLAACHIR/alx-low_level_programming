9290b094ecfdff5ab16e3b1f92378cad9d22ab4a
Anouar LAACHIR <become.engineer88@gmail.com>
2023-03-21 16:30:52 -0700

    la3b

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - a function that returns the last character of a number 
+ * print_last_digit - a function that returns the last character of a number
  *
  * @n: Return n, -n otherwise
  * Return: n, -n otherwise
@@ -12,7 +12,7 @@ int print_last_digit(int n)
 	int m;
 
 	m = (n % 10);
-	if(m < 0)
+	if (m < 0)
 		m = (m * -1);
 	_putchar(m + '0');
 	return (m);

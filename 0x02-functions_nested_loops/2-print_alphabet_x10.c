c6c40f855254f442c403c21f60a3fec2e84ee7a8
Anouar LAACHIR <become.engineer88@gmail.com>
2023-03-21 15:10:37 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+/**
+ * print_alphabet_x10 - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: 0 always
+ * */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for(i = 0; i < 10;  i++)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	_putchar('\n');
+}

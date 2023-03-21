58665ec8d6ed255fc8e2263fcc402e077d80387b
Anouar LAACHIR <become.engineer88@gmail.com>
2023-03-21 16:52:19 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,23 +3,20 @@
 /**
  * jack_bauer - a function that prints every minute of the day of Jack Bauer
  *
- * n: Return n, -n otherwise
+ * @n: Return n, -n otherwise
  * Return: n, -n otherwise
-*/
+ */
 
 void jack_bauer(void)
 {
 	int i, j;
-	
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
 		{
 			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
 			_putchar(':');
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			_putchar(j);
 			_putchar('\n');
 		}
 	}

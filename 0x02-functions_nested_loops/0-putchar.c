3058443ea32d1c4e5cb14c3d11a9c11f0d507ed8
Anouar LAACHIR <become.engineer88@gmail.com>
2023-03-21 14:22:08 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,15 @@
  * return 0 (Always).
  */
 
-int _putchar(char c)
+int main(void) 
 {
-	return write(1, &c, 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 }

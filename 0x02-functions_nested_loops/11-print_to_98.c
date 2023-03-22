c2ea1a11506846f0859e61b84eac58a89ef0a0b3
Anouar LAACHIR <become.engineer88@gmail.com>
2023-03-22 13:40:41 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,20 @@ void print_to_98(int n)
 	{
 		for(i = n; i <= 98; i++)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
 		}
 	}
 	else 
 	{
 		for(i = n; i >= 98; i--)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
 		}
 	}
 }

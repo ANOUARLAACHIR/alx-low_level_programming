421d661eb1526e63bb9eb71066cd9e63b6ef1b38
Anouar LAACHIR <become.engineer88@gmail.com>
2023-03-22 04:06:45 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,33 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, n;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= 9; j++)
 		{
-			_putchar(i * j + '0');
-			if (j == 9)
-				continue;
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			n = (i * j);
+			if ((n / 10) > 0)
+			{
+				_putchar((n / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((n % 10) + '0');
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
+
 }

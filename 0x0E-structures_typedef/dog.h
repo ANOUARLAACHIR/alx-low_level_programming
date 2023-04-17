02570a053d38b347545546aed5aaa9e86083d079
Anouar LAACHIR <become.engineer88@gmail.com>
2023-04-17 16:21:05 -0700

    up dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - list dates about pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
-}
+	char *owner;
+};
